cdca12357d87a1b989cf83b5dbccd43bec59ef8d
vinnilmg <vini.delimagomes@gmail.com>
2024-04-07 12:08:07 -0300

    otimizando consultas no db

--- src/main/java/br/com/vinnilmg/adopet/api/repository/AdocaoRepository.java
+++ src/main/java/br/com/vinnilmg/adopet/api/repository/AdocaoRepository.java
@@ -1,8 +1,9 @@
 package br.com.vinnilmg.adopet.api.repository;
 
 import br.com.vinnilmg.adopet.api.model.Adocao;
+import br.com.vinnilmg.adopet.api.model.StatusAdocao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdocaoRepository extends JpaRepository<Adocao, Long> {
-
+    boolean existsByPetIdAndStatus(Long idPet, StatusAdocao status);
 }
